b0f7b6d11dc8c2243bea7cca23e4630092f37a7c
Francelo20 <frnmed@yahoo.com.br>
2022-09-04 12:52:34 -0300

    atualizado

--- requestchama.js
+++ requestchama.js
@@ -1,19 +1,19 @@
 let moed =[
     /**/"1INCH","AAVE","ACA","ACH","ACM","ADA","ADX","AGLD","AION","AKRO","ALCX","ALGO","ALICE","ALPACA","ALPHA",
-        "ALPINE","AMP","ANC","ANKR","ANT","ANY","API3","AR","ARDR","ARPA","ASR","ASTR","ATA","ATM","ATOM","AUCTION","AUD","AUDIO",
-        "AUTO","AVA","AVAX","AXS","BADGER","BAKE","BAL","BAND","BAR","BAT","BCH","BEAM","BEL","BETA","BICO","BLZ",
-        "BNB","BNT","BNX","BOND","BTC","BTCST","BTG","BTS","BTT","BTTC","BURGER", "BUSD","C98","CAKE","CELO","CELR",
+        "ALPINE","AMP","ANC","ANKR","ANT","ANY","APE","API3","AR","ARDR","ARPA","ASR","ASTR","ATA","ATM","ATOM","AUCTION","AUD","AUDIO",
+        "AUTO","AVA","AVAX","AXS","BADGER","BAKE","BAL","BAND","BAR","BAT","BCH","BEAM","BEL","BETA","BICO","BIFI","BLZ",
+        "BNB","BNT","BNX","BOND","BSW","BTC","BTCST","BTG","BTS","BTT","BTTC","BURGER", "BUSD","C98","CAKE","CELO","CELR",
         "CFX","CHESS","CHR","CHZ","CITY","CKB","CLV","COCOS","COMP","COS","COTI","CRV","CTK","CTSI","CTXC","CVC","CVP",
         "CVX","DAR","DASH","DATA","DCR","DEGO","DENT","DEXE","DF","DGB","DIA","DNT","DOCK","DODO","DOGE","DOT","DREP",
-        "DUSK","DYDX","EGLD","ELF","ENJ","ENS","EOS","EPS","ERN","ETC","ETH","EUR","FARM","FET","FIDA","FIL","FIO","FIRO",
-        "FIS","FLM","FLOW","FLUX","FOR","FORTH","FRONT","FTM","FTT","FUN","FXS","GALA","GBP","GHST","GLMR","GNO","GRT","GTC",
+        "DUSK","DYDX","EGLD","ELF","ENJ","ENS","EOS","EPS","EPX","ERN","ETC","ETH","EUR","FARM","FET","FIDA","FIL","FIO","FIRO",
+        "FIS","FLM","FLOW","FLUX","FOR","FORTH","FRONT","FTM","FTT","FUN","FXS","GAL","GALA","GBP","GHST","GLMR","GMT","GNO","GRT","GTC",
         "GTO","GXS","HARD","HBAR","HIGH","HIVE","HNT","HOT","ICP","ICX","IDEX","ILV","IMX","INJ","IOST","IOTA","IOTX","IRIS",
-        "JASMY","JOE","JST","JUV","KAVA","KEEP","KEY","KLAY","KMD","KNC","KP3R","KSM","LAZIO","LINA","LINK","LIT","LOKA","LPT",
-        "LRC","LSK","LTC","LTO","LUNA","MANA","MASK","MATIC","MBL","MBOX","MC","MDT","MDX","MFT","MINA","MIR","MITH","MKR","MLN",
-        "MOVR","MTL","NANO","NBS","NEAR","NEO","NKN","NMR","NU","NULS","OCEAN","OG","OGN","OM","OMG","ONE","ONG","ONT","OOKI","ORN",
+        "JASMY","JOE","JST","JUV","KAVA","KDA","KEEP","KEY","KLAY","KMD","KNC","KP3R","KSM","LAZIO","LDO","LEVER","LINA","LINK","LIT","LOKA","LPT",
+        "LRC","LSK","LTC","LTO","LUNA","MANA","MASK","MATIC","MBL","MBOX","MC","MDT","MDX","MFT","MINA","MIR","MITH","MKR","MLN","MOB",
+        "MOVR","MTL","MULTI","NANO","NBS","NBT","NEAR","NEO","NEXO","NKN","NMR","NU","NULS","OCEAN","OG","OGN","OM","OMG","ONE","ONG","ONT","OOKI","OP","ORN",
         "OXT","PAXG","PEOPLE","PERL","PERP","PHA","PLA","PNT","POLS","POLY","POND","PORTO","POWR","PSG","PUNDIX","PYR","QI","QNT",
-        "QTUM","QUICK","RAD","RAMP","RARE","RAY","REEF","REN","REP","REQ","RGT","RIF","RLC","RNDR","ROSE","RSR","RUNE","RVN","SAND",
-        "SANTOS","SC","SCRT","SFP","SHIB","SKL","SLP","SNX","SOL","SPELL","SRM","STMX","STORJ","STPT","STRAX","STX","SUN","SUPER",
+        "QTUM","QUICK","RAD","RAMP","RARE","RAY","REEF","REI","REN","REP","REQ","RGT","RIF","RLC","RNDR","ROSE","RSR","RUNE","RVN","SAND",
+        "SANTOS","SC","SCRT","SFP","SHIB","SKL","SLP","SNX","SOL","SPELL","SRM","STEEM","STG","STMX","STORJ","STPT","STRAX","STX","SUN","SUPER",
         "SUSD","SUSHI","SXP","SYS","T","TCT","TFUEL","THETA","TKO","TLM","TOMO","TORN","TRB","TRIBE","TROY","TRU","TRX","TUSD","TVK",
         "TWT","UMA","UNFI","UNI","USDC","USDP","UST","UTK","VET","VGX","VIDT","VITE","VOXEL","VTHO","WAN","WAVES","WAXP","WIN","WING",
         "WNXM","WOO","WRX","WTC","XEC","XEM","XLM","XMR","XNO","XRP","XTZ","XVG","XVS","YFI","YFII","YGG","ZEC","ZEN","ZIL","ZRX"
@@ -36,21 +36,21 @@ let preco240=''
 //criando lista p fazer suporte e res a partir dos gelosss
 let lisgelos=[]
 
+/*
 setInterval(() => {
     for(nn=0;nn<=moed.length-1;nn++){
         relagelo(moed,nn)
     }  
 }, 65000); //290000
+*/
 
 
-
-/*
 setTimeout(() => {
     for(nn=0;nn<=moed.length-1;nn++){
         relagelo(moed,nn)
     }
-}, 65000);
-*/
+}, 100000);
+
     
     
 
